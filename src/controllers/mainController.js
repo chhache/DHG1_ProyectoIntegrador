abfbff7284da396869bdf7890210ce8a9d7d5d63
emanuelchamorro <emanuel.chamorro@gmail.com>
2021-11-24 21:27:01 -0300

    Sprint 3 JSON

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -1,4 +1,6 @@
-const path = require('path')
+const path = require('path');
+const fs = require('fs');
+
 
 const mainController = {
     index: function (req,res){
@@ -8,7 +10,10 @@ const mainController = {
         res.render('users/register')
     },
     productDetail: function (req,res){
-        res.render('products/productDetail')
+        let id = req.params.id //id se refiere al :id
+        const productoListado = listadoProductos() //traigo de nuevo el listado
+        let productoId = productoListado.find(el => el.id == id) //busco un solo elemento del array que tenga el id
+        res.render('products/productDetail', {productoId})
     },
     productCart:function (req,res){
         res.render('products/productCart')
@@ -20,5 +25,11 @@ const mainController = {
         res.render('index')
     }
 }
+
+//Funcion para transofmar el archivo JSON 
+const listadoProductos = function () {
+    let listadoJson = fs.readFileSync(path.resolve(__dirname, '../db/listadoProductos.json'),'utf-8');
+    return JSON.parse(listadoJson)
+  }
 
 module.exports = mainController
--- src/routes/mainRoutes.js
+++ src/routes/mainRoutes.js
@@ -4,7 +4,7 @@ const mainController = require('../controllers/mainController')
 
 router.get('/', mainController.index );
 router.get('/register', mainController.register );
-router.get('/productDetail', mainController.productDetail );
+router.get('/productDetail/:id', mainController.productDetail );
 router.get('/productCart', mainController.productCart );
 router.get('/login', mainController.login );
 router.post('/register', mainController.redirect );
